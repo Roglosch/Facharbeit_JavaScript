3c6029801a6719a1a7306085d569f60295b5bca4
Roglosch <Aki-1002BF@t-onine.de>
2020-12-13 17:37:12 +0100

    GameBasics Work

--- Test/default.js
+++ Test/default.js
@@ -1,49 +1,83 @@
 "use strict";
+let interval;
+const imgPath = "../Img/";
+const imgSrc = [
+    {src: "orc.jpg", name: "Orc"},
+    {src: "dwarf.png", name: "Dwarf"},
+    {src: "elf.png", name: "Elf"}
+];
+let activeImage;
 
-function randomPositionValue() {
-    return Math.floor(Math.random() *500) +1;
+function getRandomValue(range, min) {
+    return Math.floor(Math.random() * range) + min;
 }
 
-function randomSizeValue() {
-    return Math.floor(Math.random()*40) + 5;
-}
-
-function win(value) {
-    if (value >= 10){
+function checkAndHandleWin(value) {
+    if (value >= 10) {
         window.location.replace("./WinPage.html")
         // Es wird die Siegesbedingung geprüft und bei Erfolg die neue Seite aufgerufen
+        return true
     }
+    return false
 }
 
-function setNewButtonPosition(){
-    let valueX = randomPositionValue()
-    let valueY = randomPositionValue()
-    // Es werden mit Hilfe der Funkrion neue Werte für die Position bestimmt
+function setNewButtonPosition(element) {
+    let valueX = getRandomValue(500, 1)
+    let valueY = getRandomValue(500, 1)
+    // Es werden mit Hilfe der Funktion neue Werte für die Position bestimmt
 
-    let valueSize = randomSizeValue()
-    // Es wird mit Hilfe der Funkrion ein neuer Wert für die Größe bestimmt
+    let valueSize = getRandomValue(40, 5)
+    // Es wird mit Hilfe der Funktion ein neuer Wert für die Größe bestimmt
 
-    let element = document.getElementById('imgTarget');
-    element.style.left =valueX + 'px';
-    element.style.top  = valueY + 'px';
+    element.style.left = valueX + 'px';
+    element.style.top = valueY + 'px';
     element.style.height = valueSize + '%';
     // Die oben bestimmten Werte werden eingesetzt
 }
 
 function handleTargetClick() {
     const headerTargetHits = document.getElementById('headerHits');
-    setNewButtonPosition(headerTargetHits);
-    headerTargetHits.innerText++;
-    // Der Trefferzähler wird um 1 erhöht
-    win(headerTargetHits.innerText);
+    const headerLifeCount = document.getElementById("headerLife");
+
+    switch (activeImage) {
+        case "Dwarf":
+            headerLifeCount.innerText--;
+            break;
+        case "Orc":
+            headerTargetHits.innerText++;
+            break;
+        case "Elf":
+            headerTargetHits.innerText = (Number(headerTargetHits.innerText)+2).toString();
+            break;
+        default:
+            headerTargetHits.innerText++;
+    }
+    //TODO: Live = 0
+    checkAndHandleWin(headerTargetHits.innerText);
     // Die Funktion zu Überprüfung zum Gewinnen wird aufgerufen
-    setTimeout(()=> handleTargetClick(),250)
 }
 
-// function handleStartGame() {
-//     const headerTargetHits = document.getElementById('headerHits');
-//
-//     setNewButtonPosition(headerTargetHits);
-//
-//
-// }
+function startGame(liveCount) {
+    //TODO: Set Lives to LifeCount
+    interval = setInterval(runThroughGameLoopOnce, 1000)
+}
+
+function runThroughGameLoopOnce() {
+    console.log("One loop")
+    for (let i = 1; i <= 3; i++) {
+        const targetImage = document.getElementById("imgTarget"+i)
+        setNewButtonPosition(targetImage);
+
+        changeImage(targetImage)
+    }
+}
+
+function changeImage(targetImage) {
+    const imgNumber = Math.floor(Math.random() * imgSrc.length)
+    activeImage = imgSrc[imgNumber].name
+    targetImage.src = imgPath + imgSrc[imgNumber].src
+}
+
+function endGame() {
+    clearInterval(interval)
+}
